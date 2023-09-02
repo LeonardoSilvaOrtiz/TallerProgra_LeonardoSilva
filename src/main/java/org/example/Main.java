010fb22c6d60564527e3f85a4e0b6d430b44410f
LeonardoSilvaOrtiz <leonardo.silvaor@correoaiep.cl>
2023-09-02 11:29:37 -0400

    Modif main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        EmpleadoIndefinido empleadoIndefinido = new EmpleadoIndefinido("jona","cea",300000,13);
-        empleadoIndefinido.Informacion();
+        try{
+            EmpleadoIndefinido empleadoIndefinido = new EmpleadoIndefinido("jona","cea",300000,13);
+            empleadoIndefinido.Informacion();
 
-        int finiquitoIndefinido = empleadoIndefinido.pagarFiniquito();
-        System.out.println("Finiquito: $" + finiquitoIndefinido);
-        System.out.println("Días de vacaciones: "+empleadoIndefinido.darVacaciones());
+            int finiquitoIndefinido = empleadoIndefinido.pagarFiniquito();
+            System.out.println("Finiquito: $" + finiquitoIndefinido);
+            System.out.println("Días de vacaciones: "+empleadoIndefinido.darVacaciones());
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("Error ingreso" + e);
+
+        }
+
 
 
     }
